5b9eb877692ded29cb53a48a12e19106e9ac36d2
stillfreddie <stillfreddie1987@gmail.com>
2025-06-18 17:31:02 +0100

    Rebuilt Go app with layout + templates

--- main.go
+++ main.go
@@ -2,18 +2,43 @@ package main
 
 import (
     "html/template"
+    "log"
     "net/http"
+    "path/filepath"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("templates/index.html"))
-    tmpl.Execute(w, nil)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+    layout := filepath.Join("templates", "layout.html")
+    page := filepath.Join("templates", tmpl+".html")
+
+    templates, err := template.ParseFiles(layout, page)
+    if err != nil {
+        http.Error(w, "Error loading template", http.StatusInternalServerError)
+        log.Println("Template error:", err)
+        return
+    }
+
+    templates.Execute(w, nil)
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        renderTemplate(w, "home")
+    })
+    http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+        renderTemplate(w, "about")
+    })
+    http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
+        renderTemplate(w, "contact")
+    })
+    http.HandleFunc("/admin", func(w http.ResponseWriter, r *http.Request) {
+        renderTemplate(w, "admin")
+    })
 
-    println("🚀 Server is running at http://localhost:8080")
+    // Serve static files (CSS, images)
+    fs := http.FileServer(http.Dir("static"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+    log.Println("Server is running on http://localhost:8080")
     http.ListenAndServe(":8080", nil)
 }
